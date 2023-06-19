6f8194ef20c0d32d0ecd6f4e4696ed306e8e913e
ravielz <ravielz20@gmail.com>
2023-06-19 15:15:28 +0700

    progress model

--- impl/fetcher/model.py
+++ impl/fetcher/model.py
@@ -11,17 +11,50 @@ STREAM_FILE_PATH = 'data.txt'
 
 class PredictComponent:
 
-    def __init__(self, data):
-        self.automodel = auto_arima(data, trace=False, suppress_warnings=True)
-        self.order = self.automodel.order
-        self.model = ARIMA(data, order=self.order)
-        self.trained_model = self.model.fit()
+    def __init__(self, data, treshold=0.35):
+        self.data = data.copy()
+        self.treshold = treshold
+        self.train()
     
     def predict(self, step):
         return self.trained_model.forecast(steps=step)
     
+    def forecast(self, second):
+        p = self.trained_model.forecast(steps=second)
+        return (p.index[-1], p.iloc[-1])
+    
+    def train(self):
+        start = time.time()
+        self.automodel = auto_arima(self.data, trace=False, suppress_warnings=True, maxiter=1000, max_p=25, max_q=25, max_P=25, max_Q=25, stepwise=True)
+        self.order = self.automodel.order
+        self.model = ARIMA(self.data, order=self.order)
+        self.trained_model = self.model.fit()
+        end = time.time()
+        print(f"Training time: {round((end-start)*1000,3)}ms")
+        self.next_retrain = int(len(self.data) * self.treshold)
+    
+    def add_data(self, new_data):
+        new_data_copied = new_data.copy()
+        self.data = pd.concat([self.data, new_data_copied])
+        self.train()
+    
+    def update(self, new_data):
+        self.next_retrain = self.next_retrain - len(new_data)
+        if self.next_retrain < 0:
+            self.add_data(new_data)
+        else:
+            start = time.time()
+            new_data_copied = new_data.copy()
+            self.data = pd.concat([self.data, new_data_copied])
+            self.model.update(new_data_copied)
+            end = time.time()
+            print(f"Update time: {round((end-start)*1000,3)}ms, Next Retrain: {self.next_retrain}")
+    
     def last_index(self):
         return self.trained_model.nobs
+    
+    def __str__(self):
+        return f"ARIMA {self.order}"
 
 # np.random.seed(0)
 # data = pd.Series(np.random.randn(30), index=pd.date_range('2000-01-01', periods=30))
@@ -72,17 +105,18 @@ def create_dataframe(data):
     #print(df.to_string())
     return df
 
-data = []
+data = read_from_file()
+df = create_dataframe(data)
+model_cpupercent = PredictComponent(df['CPUPercent'])
 while True:
     currdata = read_from_file()
     if len(currdata) > 0:
+        currdf = create_dataframe(currdata)
+        model_cpupercent.update(currdf['CPUPercent'])
         for each in currdata:
             data.append(each)
     else:
-        print(f"Skipping training, current data={len(data)}")
-        time.sleep(10)
-        continue
-    df = create_dataframe(data)
-    x = PredictComponent(df['CPUPercent'])
-    print(x.predict(60))
+        print(f"No new data found, current data={len(data)}")
+    print(f"Next 60s: {model_cpupercent.forecast(60)}")
+    print(f"Next 120s: {model_cpupercent.forecast(120)}")
     time.sleep(5)
